379d4782b3b418012c0124d469cd9862d2e1d584
_YOUSAN_NIM <ptolemaios.ong@gmail.com>
2024-08-07 16:25:48 +0700

    Updated

--- src/app/central-beuaty-award/page.tsx
+++ src/app/central-beuaty-award/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import { PointLight } from 'three';
@@ -12,11 +12,48 @@ import Tropgy from './Trophy';
 // <!-- #dcbb9a -->
 // <!-- #b18b68 -->
 
+
+
+interface ButtonProps {
+    onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
 const CentralBeuatyAward = () => {
 
     // const [cardType, setCardType] = useState()
     const lightRef = useRef<PointLight>(null);
+
+    const [isTouch, setIstouch] = useState(false)
     // bg-[#F6E7DF]
+
+
+
+
+
+
+
+
+    const CliclHandler = () => {
+        setIstouch(!isTouch)
+    }
+
+
+    useEffect(() => {
+
+        // setTimeout(() => {
+        //     setIstouch(!isTouch)
+        //     console.log("text")
+        // }, 2000)
+
+        if (isTouch) {
+            setTimeout(() => {
+                setIstouch(!isTouch)
+                console.log("text")
+            }, 30000)
+        }
+
+    },[isTouch])
+
     return (
         <div className='relative w-screen h-screen overflow-hidden bg-black'>
 
@@ -24,30 +61,44 @@ const CentralBeuatyAward = () => {
                 CENTRAL 
             </div> */}
 
-            <div className='absolute z-20 w-full h-full opacity-60'>
-                <video
-                    loop
-                    muted
-                    autoPlay
-                    preload='none'
-                    className='w-full'
-                >
-                    <source
-                        src="/MICROSITES_CREaiVE/mp4/award.mp4"
-                        type="video/mp4"
-                    />
-                </video>
-            </div>
 
-            <div className='absolute z-30 w-full text-gray text-center top-[55%] opacity-loop-animate'>
-                <div className='flex text-[50px] justify-center items-center w-[300px] h-[300px] border-4 rounded-[100%] font-light font-serif text-gray-200 border-gray-200 m-auto bg-black bg-opacity-50 uppercase'>
-                    Click !
-                </div>
-            </div>
 
-            <div>
+            {
+                isTouch ? (
+                    <div>
+                        {/* selecting coins */}
 
-            </div>
+                        <div className="absolute text-[120px] w-full text-center top-[30%]">
+                            "ไม่มีคนทำต่อ รอไปก่อน"
+                        </div>
+                    </div>
+                ) : (
+                    <div>
+                        <div className='absolute z-20 w-full h-full opacity-60'>
+                            <video
+                                loop
+                                muted
+                                autoPlay
+                                preload='none'
+                                className='w-full'
+                            >
+                                <source
+                                    src="/MICROSITES_CREaiVE/mp4/award.mp4"
+                                    type="video/mp4"
+                                />
+                            </video>
+                        </div>
+
+                        <div className='absolute z-30 w-full text-gray text-center top-[55%] opacity-loop-animate'>
+                            <button onClick={CliclHandler}>
+                                <div className='flex text-[50px] justify-center items-center w-[300px] h-[300px] border-4 rounded-[100%] font-light font-serif text-gray-200 border-gray-200 m-auto bg-black bg-opacity-50 uppercase'>
+                                    Click !
+                                </div>
+                            </button>
+                        </div>
+                    </div>
+                )
+            }
 
 
             {/* <div className='absolute w-full h-full z-50 '>
