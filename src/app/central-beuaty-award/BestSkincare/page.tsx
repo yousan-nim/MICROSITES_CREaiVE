1c4aed11116f68841ee5d5ca38e152e637ce3384
_YOUSAN_NIM <65081261+IZZARA-URA@users.noreply.github.com>
2024-08-14 00:17:01 +0700

    Updated Image BestSerum

--- src/app/central-beuaty-award/BestSkincare/page.tsx
+++ src/app/central-beuaty-award/BestSkincare/page.tsx
@@ -1,37 +1,26 @@
 import React from "react";
+import { data } from "./data"
 
-
-
-const DATA_PAGE = {
-  title: "Best Eco-Friendly",
-  description: "Central Bauety Award 2024",
-  image: "/MICROSITES_CREAIVE/img/CentralBauetyAward2024/CentralBauetyAward2024.png",
-  alt: "Central Bauety Award 2024",
-  shareTitle: "Central Bauety Award 2024 - Best Eco-Friendly",
-  shareDescription:
-    "Discover the best eco-friendly products and brands that are part of Central Bauety Award 2024.",
-  shareImage: "/MICROSITES_CREAIVE/img/CentralBauetyAward2024/CentralBauetyAward2024.png",
-  shareUrl: "https://centralbauetyaward.com/best-eco-friendly",
-}
-
-
-
-const BestEcoFriendly = () => {
+function BestEcoFriendly() {
   return (
     <div className="relative w-screen h-screen overflow-hidden bg-white ">
       <div className="relative w-screen h-screen items-center text-center text-[200px] m-auto">
         <div className="flex w-full m-auto items-center justify-center">
-          <img
-            src="/MICROSITES_CREaiVE/img/CentralBauetyAward2024/CentralBauetyAward2024.png"
-          />
+          <img src="/MICROSITES_CREaiVE/img/CentralBauetyAward2024/CentralBauetyAward2024.png" />
         </div>
 
         <div className="relative w-screen h-full">
-          <img
-            src="/MICROSITES_CREaiVE/img/CentralBauetyAward2024/Trophys/SkinCare.png"
-          />
-
+          <img src={data.dataTrophy} />
         </div>
+
+
+        {
+          data.data.BestEssence.first.name
+        }
+
+        
+
+
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,34 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="w-full text-black text-[200px] text-center">
-        TEST Deployment 
+        TEST Deployment
+      </div>
+
+      <div className="w-full text-black m-auto grid grid-cols-4 gap-4">
+        <div className="border-2 p-4 rounded-xl">
+          <Link href="/central-beuaty-award">
+            Centrall Beauty Award 2024
+          </Link>
+        </div>
+        <div className="border-2 p-4 rounded-xl">
+          <Link href="/central-beuaty-award">
+            Centrall Beauty Award 2024
+          </Link>
+        </div>
+        <div className="border-2 p-4 rounded-xl">
+          <Link href="/central-beuaty-award">
+            Centrall Beauty Award 2024
+          </Link>
+        </div>
+        <div className="border-2 p-4 rounded-xl">
+          <Link href="/central-beuaty-award">
+            Centrall Beauty Award 2024
+          </Link>
+        </div>
       </div>
     </main>
   );
