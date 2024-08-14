899fdc9d6c0fa4d3f066b5737c97a18c9736d625
_YOUSAN_NIM <65081261+IZZARA-URA@users.noreply.github.com>
2024-08-14 22:53:54 +0700

    Update Finished Best Luxury

--- src/app/central-beuaty-award/BestLuxury/page.tsx
+++ src/app/central-beuaty-award/BestLuxury/page.tsx
@@ -1,35 +1,67 @@
 "use client"
-import React from "react";
-import { data } from "./data"
+import React, { useEffect, useState, FC } from 'react'
 import { useRouter } from 'next/navigation'
 
-import Link from "next/link"
+import { data } from "./data"
 import { MdOutlineArrowBack } from "react-icons/md";
 import { MdHome } from "react-icons/md";
 
 
-type Props = {
-  image: string
-  name: string
-  href_to: string
-}
-
-const First_ProductBox = ({ image, name, href_to }: Props) => {
-  return (
-    <Link href={href_to}>
-      <img src={image}
-        className="w-[400px] h-[400px] z-20 top-[1200px]"
-      />
-      <div className="text-black text-[18px] p-4 ">
-        {name}
-      </div>
-    </Link>
-  )
-}
-
-
-function BestLuxury() {
+const page = () => {
   const router = useRouter()
+
+  const [hero, setHero] = useState({
+    img: data.data.BestLuxury.first.image,
+    name: data.data.BestLuxury.first.name,
+    qr: data.data.BestLuxury.first.imageQr
+  })
+
+  const FirstClick = () => {
+    setHero({
+      img: data.data.BestLuxury.first.image,
+      name: data.data.BestLuxury.first.name,
+      qr: data.data.BestLuxury.first.imageQr
+    })
+  }
+
+  const SecondClick = () => {
+    setHero({
+      img: data.data.BestLuxury.second.image,
+      name: data.data.BestLuxury.second.name,
+      qr: data.data.BestLuxury.second.imageQr
+    })
+  }
+
+  const ThirdClick = () => {
+    setHero({
+      img: data.data.BestLuxury.third.image,
+      name: data.data.BestLuxury.third.name,
+      qr: data.data.BestLuxury.third.imageQr
+    })
+  }
+
+  const FourthClick = () => {
+    setHero({
+      img: data.data.BestLuxury.fourth.image,
+      name: data.data.BestLuxury.fourth.name,
+      qr: data.data.BestLuxury.fourth.imageQr
+    })
+  }
+
+  const FifthClick = () => {
+    setHero({
+      img: data.data.BestLuxury.fifth.image,
+      name: data.data.BestLuxury.fifth.name,
+      qr: data.data.BestLuxury.fifth.imageQr
+    })
+  }
+
+
+  useEffect(() => {
+
+    console.log(hero)
+
+  }, [hero])
 
   return (
     <div className="relative w-screen h-screen overflow-hidden bg-white ">
@@ -38,48 +70,73 @@ function BestLuxury() {
           <img src="/MICROSITES_CREaiVE/img/CentralBauetyAward2024/CentralBauetyAward2024.png" />
         </div>
 
-        <div className='absolute z-20 text-black text-[18px] w-[100px] h-[100px] top-[50%] p-4'>
+        <div className='absolute text-black text-[18px] w-[100px] h-[100px] top-[50%] p-4'>
           <MdHome size={80} onClick={() => router.push('/central-beuaty-award')} color='#e4c7ad' className='m-4 border-2 border-[#e4c7ad] rounded-[100px]' />
           <MdOutlineArrowBack size={80} onClick={() => router.back()} color='#e4c7ad' className='m-4 border-2 border-[#e4c7ad] rounded-[100px]' />
         </div>
 
-        <div className="absolute z-10 flex top-[25%] w-full m-auto font-gothamBook uppercase">
-          <div className="grid grid-cols-12 justify-center items-center m-auto gap-10">
-            <div className="col-start-4">
-              {/*  */}
+        <div className='absolute z-50 right-0 w-[250px] text-black top-[16vh] p-2'>
+          <div onClick={FirstClick} className='border-2 rounded-[20px] shadow-xl '>
+            <img src={data.data.BestLuxury.first.image} />
+            <div className='text-[14px] pt-4 px-2 font-gothamBook uppercase'>
+              {data.data.BestLuxury.first.name}
             </div>
-            <div className="col-start-3 col-span-8 items-center justify-center m-auto">
-              <div className="flex text-[40px] ">
-                <First_ProductBox image={data.data.BestEssence.first.image} name={data.data.BestEssence.name} href_to={data.data.BestEssence.href} />
-                <First_ProductBox image={data.data.BestSuncreen.first.image} name={data.data.BestSuncreen.name} href_to={data.data.BestSuncreen.href} />
-              </div>
-            </div>
+          </div>
 
-            <div className="col-start-2 col-span-3 text-[40px]">
-              <First_ProductBox image={data.data.BestMenSkincare.first.image} name={data.data.BestMenSkincare.name} href_to={data.data.BestMenSkincare.href} />
+          <div onClick={SecondClick} className='pt-4 mt-4  border-2 rounded-[20px] shadow-xl'>
+            <img src={data.data.BestLuxury.second.image} />
+            <div className='text-[14px] pt-4 px-2 font-gothamBook uppercase'>
+              {data.data.BestLuxury.second.name}
             </div>
-            <div className="col-end-12 col-span-3 text-[40px]">
-              <First_ProductBox image={data.data.BestSerum.first.image} name={data.data.BestSerum.name} href_to={data.data.BestSerum.href} />
-            </div>
+          </div>
 
-            <div className="col-start-3 col-span-3 text-[40px]">
-              <First_ProductBox image={data.data.BestMoisturizer.first.image} name={data.data.BestMoisturizer.name} href_to={data.data.BestMoisturizer.href} />
+          <div onClick={ThirdClick} className='pt-4 mt-4  border-2 rounded-[20px] shadow-xl'>
+            <img src={data.data.BestLuxury.third.image} />
+            <div className='text-[14px] pt-4 px-2 font-gothamBook uppercase'>
+              {data.data.BestLuxury.third.name}
             </div>
+          </div>
 
-            <div className="col-end-11 col-span-3 text-[40px]">
-              <First_ProductBox image={data.data.BestEyeCream.first.image} name={data.data.BestEyeCream.name} href_to={data.data.BestEyeCream.href} />
+          <div onClick={FourthClick} className='pt-4 mt-4  border-2 rounded-[20px] shadow-xl'>
+            <img src={data.data.BestLuxury.fourth.image} />
+            <div className='text-[14px] pt-4 px-2 font-gothamBook uppercase'>
+              {data.data.BestLuxury.fourth.name}
+            </div>
+          </div>
+
+          <div onClick={FifthClick} className='pt-4 mt-4  border-2 rounded-[20px] shadow-xl'>
+            <img src={data.data.BestLuxury.fifth.image} />
+            <div className='text-[14px] pt-4 px-2 font-gothamBook uppercase'>
+              {data.data.BestLuxury.fifth.name}
             </div>
           </div>
         </div>
 
+        {/* Hero image */}
+        <div className='relative m-auto pt-10 justify-center items-center text-center w-[50vw]'>
+          <img src={hero.img}
+            className='w-[80vw] m-auto'
+          />
 
-        <div className="relative w-screen h-full top-[-16%]">
-          <img src={data.dataTrophy} className="z-0" />
+          {/* Text */}
+          <div className='text-black text-[30px] border-2 rounded-[20px] w-[50vw] m-auto shadow-xl p-4 font-centrallMeduim uppercase'>
+            {hero.name}
+          </div>
+
         </div>
 
+        {/* QR code */}
+        <div className='w-full m-auto text-black pt-10 '>
+          <img src={hero.qr}
+            className='w-[30%] m-auto border-2 rounded-[20px] shadow-xl p-4'
+          />
+          <div className='text-[20px] pt-4 font-gothamBook uppercase'>
+            BUY HERE!
+          </div>
+        </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default BestLuxury;
+export default page
