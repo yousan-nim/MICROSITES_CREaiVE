d0e8041224536cbbde0aea8a4a696a28665f957a
_YOUSAN_NIM <ptolemaios.ong@gmail.com>
2024-08-16 18:23:50 +0700

    Updated all layout mobile

--- src/app/central-beuaty-award/BestMakeUp/BestFoundation/page.tsx
+++ src/app/central-beuaty-award/BestMakeUp/BestFoundation/page.tsx
@@ -70,61 +70,71 @@ const page = () => {
           <img src="/MICROSITES_CREaiVE/img/CentralBauetyAward2024/CentralBauetyAward2024.png" />
         </div>
 
-        <div className='absolute text-black text-[18px] w-[100px] h-[100px] top-[50%] p-4'>
-          <MdHome size={80} onClick={() => router.push('/central-beuaty-award')} color='#e4c7ad' className='m-4 border-2 border-[#e4c7ad] rounded-[100px]' />
-          <MdOutlineArrowBack size={80} onClick={() => router.back()} color='#e4c7ad' className='m-4 border-2 border-[#e4c7ad] rounded-[100px]' />
+        <div className="absolute z-0 hv:z-20 text-black text-[18px] w-screen h-[10vh] top-[85%] hv:top-[30%] flex hv:flex-none hv:justify-start justify-around p-4">
+          <MdOutlineArrowBack
+            size={80}
+            onClick={() => router.back()}
+            color="#e4c7ad"
+            className="m-4 border-2 border-[#e4c7ad] rounded-[100px]"
+          />
+          <MdHome
+            size={80}
+            onClick={() => router.push("/central-beuaty-award")}
+            color="#e4c7ad"
+            className="m-4 border-2 border-[#e4c7ad] rounded-[100px] "
+          />
         </div>
 
-        <div className='absolute z-50 right-8 w-[250px] text-black top-[20vh] p-4 pt-0 '>
+        <div className='absolute z-50 right-1 hv:right-8 w-[20vw] hv:w-[250px] text-black top-[20vh] hv:p-4 hv:pt-0 '>
           <div onClick={FirstClick} className='border-2 rounded-[20px] shadow-xl '>
-            <img src={data.data.BestFoundation.first.image} className='pt-4'/>
-            <div className='text-[14px] pt-4 px-2 font-gothamBook uppercase'>
+            <img src={data.data.BestFoundation.first.image} className='hv:pt-4' />
+            <div className='hidden hv:text-[14px] hv:pt-4 hv:px-2 font-gothamBook uppercase'>
               {data.data.BestFoundation.first.name}
             </div>
           </div>
 
           <div onClick={SecondClick} className='pt-4 mt-4  border-2 rounded-[20px] shadow-xl'>
-            <img src={data.data.BestFoundation.second.image} className='pt-4'/>
-            <div className='text-[14px] pt-4 px-2 font-gothamBook uppercase'>
+            <img src={data.data.BestFoundation.second.image} className='hv:pt-4' />
+            <div className='hidden hv:text-[14px] hv:pt-4 hv:px-2 font-gothamBook uppercase'>
               {data.data.BestFoundation.second.name}
             </div>
           </div>
 
           <div onClick={ThirdClick} className='pt-4 mt-4  border-2 rounded-[20px] shadow-xl'>
-            <img src={data.data.BestFoundation.third.image} className='pt-4'/>
-            <div className='text-[14px] pt-4 px-2 font-gothamBook uppercase'>
+            <img src={data.data.BestFoundation.third.image} className='hv:pt-4' />
+            <div className='hidden hv:text-[14px] hv:pt-4 hv:px-2 font-gothamBook uppercase'>
               {data.data.BestFoundation.third.name}
             </div>
           </div>
 
           <div onClick={FourthClick} className='pt-4 mt-4  border-2 rounded-[20px] shadow-xl'>
-            <img src={data.data.BestFoundation.fourth.image} className='pt-4'/>
-            <div className='text-[14px] pt-4 px-2 font-gothamBook uppercase'>
+            <img src={data.data.BestFoundation.fourth.image} className='hv:pt-4' />
+            <div className='hidden hv:text-[14px] hv:pt-4 hv:px-2 font-gothamBook uppercase'>
               {data.data.BestFoundation.fourth.name}
             </div>
           </div>
 
           <div onClick={FifthClick} className='pt-4 mt-4  border-2 rounded-[20px] shadow-xl'>
-            <img src={data.data.BestFoundation.fifth.image} className='pt-4'/>
-            <div className='text-[14px] pt-4 px-2 font-gothamBook uppercase'>
+            <img src={data.data.BestFoundation.fifth.image} className='hv:pt-4' />
+            <div className='hidden hv:text-[14px] hv:pt-4 hv:px-2 font-gothamBook uppercase'>
               {data.data.BestFoundation.fifth.name}
             </div>
           </div>
         </div>
 
         <div className='w-[55vw] h-auto m-auto '>
-          <div className='relative m-auto pt-10 justify-center items-center text-center w-full text-black text-[40px] uppercase font-gothamBook'>
+          <div className='relative m-auto pt-10 justify-center items-center text-center w-full text-black text-[15px] hv:text-[40px] uppercase font-gothamBook'>
             {data.dataPageTitle} : {data.data.BestFoundation.name}
           </div>
 
           {/* Hero image */}
           <div className='relative m-auto pt-10 justify-center items-center text-center w-full'>
             <img src={hero.img}
-              className='w-[90vw] m-auto pb-8'
+              className='w-[90vw] m-auto pb-2 hv:pb-8'
             />
 
             {/* Text */}
-            <div className='text-black text-[25px] border-2 rounded-[20px] w-[90%] h-auto m-auto shadow-sm p-4 font-gothamBook uppercase'>
+            <div className='text-black text-[15px] hv:text-[25px] border-2 rounded-[20px] w-[90%] h-auto m-auto shadow-sm hv:p-4 font-gothamBook uppercase'>
               {hero.name}
             </div>
           </div>
@@ -134,7 +144,7 @@ const page = () => {
             <img src={hero.qr}
               className='w-[90%] m-auto border-2 rounded-[20px] shadow-sm p-4'
             />
-            <div className='text-[20px] pt-4 font-gothamBook uppercase p-2'>
+            <div className='hidden hv:text-[20px] hv:pt-4 hv:font-gothamBook hv:uppercase hv:p-2'>
               BUY HERE!
             </div>
           </div>
