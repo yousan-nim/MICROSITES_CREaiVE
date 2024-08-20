ac8fb1c70a2680b282024ec3cfeb7ce78385e1ed
_YOUSAN_NIM <ptolemaios.ong@gmail.com>
2024-08-20 17:27:54 +0700

    Update

--- src/app/central-beuaty-award/GenerateYourBauetyGiftCard/page.tsx
+++ src/app/central-beuaty-award/GenerateYourBauetyGiftCard/page.tsx
@@ -127,8 +127,8 @@ const page = () => {
                 className="w-[80px] h-[80px] border-2 border-[#e4c7ad] rounded-[100px] bg-[#F6E7DF] hv:hidden"
               />
 
-              <div className="w-[200px]">
-
+              <div className="w-[200px] text-[40px]">
+                HOLD TO DOWNLOAD
               </div>
 
               <MdHome
