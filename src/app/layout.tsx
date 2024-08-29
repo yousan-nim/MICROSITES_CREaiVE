bd795f8d1969da9ac10e94c98018b402e938d257
_YOUSAN_NIM <ptolemaios.ong@gmail.com>
2024-08-29 17:56:26 +0700

    Upodated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,8 @@ export default function RootLayout({
 
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, user-scalable=no" />
+        {/* <meta name="viewport" content="width=device-width, user-scalable=no" /> */}
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"></meta>
       </head>
 
       <body
