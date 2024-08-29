d7ac4670db922e29db4c4158f3c03110827c7d90
_YOUSAN_NIM <ptolemaios.ong@gmail.com>
2024-08-29 18:57:52 +0700

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,47 +1,57 @@
 /** @type {import('next').NextConfig} */
+
+
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
+
+
 const nextConfig = {
-    /**
-     * Enable static exports for the App Router.
-     *
-     * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-     */
-    // output: "export",
-    // output: process.env.GITHUB_ACTION ? 'export' : undefined,
-    trailingSlash: true,
-  
-    /**
-     * Set base path. This is the slug of your GitHub repository.
-     *
-     * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-     */
-    // basePath: "",
-  
-    /**
-     * Disable server-based image optimization. Next.js does not support
-     * dynamic features with static exports.
-     *
-     * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-     */
-    images: {
-      unoptimized: true,
-    },
-    // reactStrictMode: true,
-    staticPageGenerationTimeout: 1000,
-    // distDir: 'build'
+  /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  // output: "export",
+  // output: process.env.GITHUB_ACTION ? 'export' : undefined,
+  trailingSlash: true,
 
-    reactStrictMode: false,
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  // basePath: "",
 
-    async rewrites() {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'https://openapi.akool.com/api/*',
-        },
-      ]
-    },
-    
-  };
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
+  // reactStrictMode: true,
+  staticPageGenerationTimeout: 1000,
+  // distDir: 'build'
 
-  
-  
-  export default nextConfig;
+  reactStrictMode: false,
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://openapi.akool.com/api/*',
+      },
+    ]
+  },
+
+};
+
+
+
+// export default nextConfig;
+
+module.exports = withPWA(nextConfig)
