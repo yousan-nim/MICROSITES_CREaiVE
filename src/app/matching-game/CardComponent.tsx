2391144046e813a0ffaea2a710bd4c45eda5a5f6
_YOUSAN_NIM <ptolemaios.ong@gmail.com>
2024-09-24 18:25:00 +0700

    Updated

--- src/app/matching-game/CardComponent.tsx
+++ src/app/matching-game/CardComponent.tsx
@@ -13,7 +13,7 @@ const CardComponent: React.FC<Props> = ({ onClick, card }) => {
 
   return (
     <div
-      className={`w-[11.5vh] h-[11.5vh] col-span-1 rounded-xl m-auto shadow-2xl shadow-black ${card.open ? "transition-all duration-300 [transform:rotateY(180deg)] bg-white" : "bg-transparent"}`}
+      className={`w-[11.5vh] h-[11.5vh] col-span-1 rounded-[32px] m-auto shadow-2xl shadow-black bg-transparent ${card.open ? "transition-all duration-300 [transform:rotateY(180deg)] bg-white" : "bg-transparent"}`}
 
       onClick={(e) => onClick && onClick(e)}
       data-testid={card.id}
@@ -23,14 +23,14 @@ const CardComponent: React.FC<Props> = ({ onClick, card }) => {
           card.open ? <div className=''>
             <img
               src={`/assets/${card.img} `}
-              className='w-full p-0 [transform:rotateY(180deg)] rounded-xl'
+              className='w-full [transform:rotateY(180deg)] rounded-xl'
             />
           </div>
             :
             <div className={`flex text-center m-auto justify-center items-center ${card.open ? '' : "transition-all duration-300 [transform:rotateY(360deg)]"}`}>
               <img
                 src='/assets/AI Games Source-02.png'
-                className='w-full '
+                className='w-full h-full'
               />
             </div>
         }
